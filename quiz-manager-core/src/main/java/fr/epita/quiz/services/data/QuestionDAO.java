964dd4e61b8a32990823f43d053fdcd79a38ae27
tbrou <tbrou@tbrou-PC7.natsystem.fr>
2018-09-25 19:58:11 +0200

    * exercise : implement as methods as you can

---
+++ quiz-manager-core/src/main/java/fr/epita/quiz/services/data/QuestionDAO.java
@@ -0,0 +1,22 @@
+package fr.epita.quiz.services.data;
+
+public class QuestionDAO {
+	
+	public void create(Question question) {
+		
+	}
+	public void update(Question question) {
+		
+	}
+	public void delete(Question question) {
+		
+	}
+	public List<Question> search(Question criteria) {
+		
+	}
+	
+	public Question getById(Long id) {
+		
+	}
+
+}
